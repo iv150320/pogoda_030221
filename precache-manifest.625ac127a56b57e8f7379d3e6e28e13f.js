0a3af066416f0d9f3ea67392a0343ab7d4c2f580
iv150320 <johnrodin1@gmail.com>
2021-02-03 23:29:49 +0300

    Апи работает на сервере через https

---
+++ precache-manifest.625ac127a56b57e8f7379d3e6e28e13f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717129b52a6c4b69edfbd93869dd9acc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f5507d5ae63d53bcbd5",
+    "url": "./static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "f6884bf48716799fefc9",
+    "url": "./static/css/main.5b6e81ca.chunk.css"
+  },
+  {
+    "revision": "9f5507d5ae63d53bcbd5",
+    "url": "./static/js/2.18493a4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.18493a4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6884bf48716799fefc9",
+    "url": "./static/js/main.96c2f8fa.chunk.js"
+  },
+  {
+    "revision": "bd794c7b53e10c5f0bba",
+    "url": "./static/js/runtime-main.873cd961.js"
+  }
+]);
